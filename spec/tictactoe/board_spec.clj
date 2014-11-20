26206c19c1b5687da8e344952a014db978b926a2
Christoph Gockel <github@christophgockel.de>
2014-11-20 11:28:52 +0000

    Added board behaviour

--- spec/tictactoe/board_spec.clj
+++ spec/tictactoe/board_spec.clj
@@ -14,4 +14,32 @@
   (context "place-move"
     (it "indexes are one based"
       (should= ["x" 2 3 4 5 6 7 8 9]
-               (place-move "x" 1 (new-board 3))))))
+               (place-move "x" 1 (new-board 3)))))
+
+  (context "is-valid-move?"
+    (let [board (place-move "x" 1 (new-board 3))]
+      (it "returns true on valid moves"
+        (should (is-valid-move? 2 board)))
+
+      (it "returns false on invalid moves"
+        (should-not (is-valid-move? 1 board)))))
+
+  (context "winning combinations"
+    (it "identifies winning rows"
+      (should= true (has-winner? ["x" "x" "x" 4 5 6 7 8 9]))
+      (should= true (has-winner? [1 2 3 "x" "x" "x" 7 8 9]))
+      (should= true (has-winner? [1 2 3 4 5 6 "x" "x" "x"])))
+
+    (it "identifies winning columns"
+      (should= true (has-winner? ["x" 2 3 "x" 5 6 "x" 8 9]))
+      (should= true (has-winner? [1 "x" 3 4 "x" 6 7 "x" 9]))
+      (should= true (has-winner? [1 2 "x" 4 5 "x" 7 8 "x"])))
+
+    (it "identifies winning diagonals"
+      (should= true (has-winner? ["x" 2 3 4 "x" 6 7 8 "x"]))
+      (should= true (has-winner? [1 2 "x" 4 "x" 6 "x" 8 9])))
+
+    (it "identifies the winner"
+      (should= "x" (winner ["x" "x" "x" 4 5 6 7 8 9]))
+      (should= "o" (winner [1 2 "o" 4 "o" 6 "o" 8 9])))))
+
--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -5,3 +5,48 @@
 
 (defn place-move [mark position board]
   (assoc board (- position 1) mark))
+
+(defn- position-taken? [position board]
+  (string? (nth board (- position 1))))
+
+(defn is-valid-move? [position board]
+  (not (position-taken? position board)))
+
+(defn- width [board]
+  (let [length (count board)]
+    (int (Math/sqrt length))))
+
+(defn- rows [board]
+  (partition (width board) board))
+
+(defn- is-unique? [line]
+   (= 1 (count (distinct line))))
+
+(defn- columns [board]
+  (apply map list (rows board)))
+
+(defn- diagonals [board]
+  (partition
+    (width board )
+    (concat
+      (map-indexed
+        (fn [index item] (get (vec item) index))
+        (rows board))
+      (map-indexed
+        (fn [index item] (get (vec item) index))
+        (reverse (rows board))))))
+
+(defn- line-combinations [board]
+  (concat
+    (rows board)
+    (columns board)
+    (diagonals board)))
+
+(defn has-winner? [board]
+  (> (count (filter is-unique? (line-combinations board))) 0))
+
+(defn winner [board]
+  (first
+    (apply #(-> %)
+           (filter is-unique? (line-combinations board)))))
+
