38195e7d2e77b6074c5f3f17d1d68ffa4ce045cf
Christoph Gockel <github@christophgockel.de>
2014-11-21 10:29:50 +0000

    Update game spec to use (with) instead of (let) references

--- spec/tictactoe/game_spec.clj
+++ spec/tictactoe/game_spec.clj
@@ -8,21 +8,19 @@
   {:mark mark})
 
 (describe "Game"
-  (let [board (new-board 3)
-        player_a (fake-player "x")
-        player_b (fake-player "o")]
+  (with board    (new-board 3))
+  (with player_a (fake-player "x"))
+  (with player_b (fake-player "o"))
 
-    (it "plays a round"
-      (with-in-str "1"
-        (let [game (new-game player_a player_b board)
-              another-game (play-round game)]
-          (should= ["x" 2 3 4 5 6 7 8 9]
-                   (:board another-game)))))
+  (it "plays a round"
+    (let [game (new-game @player_a @player_b @board)
+          another-game (with-in-str "1" (play-round game))]
+      (should= ["x" 2 3 4 5 6 7 8 9]
+               (:board another-game))))
 
-    (it "switches players after playing a round"
-      (with-in-str "1"
-        (let [game (new-game player_a player_b board)
-              another-game (play-round game)]
-          (should= [player_b player_a]
-                   (:players another-game)))))))
+  (it "switches players after playing a round"
+    (let [game (new-game @player_a @player_b @board)
+          another-game (with-in-str "1" (play-round game))]
+      (should= [@player_b @player_a]
+               (:players another-game)))))
 
