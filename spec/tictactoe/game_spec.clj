77cad0254284afe0c7c6326cf58d94c264acb2f5
Christoph Gockel <github@christophgockel.de>
2014-11-24 09:48:57 +0000

    Adds first tests for a game

--- spec/tictactoe/game_spec.clj
+++ spec/tictactoe/game_spec.clj
@@ -2,25 +2,35 @@
   (:require [speclj.core :refer :all]
             [tictactoe.game :refer :all]
             [tictactoe.player :refer :all]
-            [tictactoe.board :refer :all]))
+            [tictactoe.board :refer :all]
+            [tictactoe.io :refer :all]))
 
 (defn fake-player [mark]
   {:mark mark})
 
 (describe "Game"
+  (with-stubs)
   (with board    (new-board 3))
   (with player_a (fake-player "x"))
   (with player_b (fake-player "o"))
+  (with game (new-game @player_a @player_b @board))
+  (with show-board-stub (stub :show-board))
+
+  (around [it]
+    (with-redefs [tictactoe.io/show-board @show-board-stub]
+      (with-in-str "1" (it))))
 
   (it "plays a round"
-    (let [game (new-game @player_a @player_b @board)
-          another-game (with-in-str "1" (play-round game))]
+    (let [another-game (play-round @game)]
       (should= ["x" 2 3 4 5 6 7 8 9]
                (:board another-game))))
 
   (it "switches players after playing a round"
-    (let [game (new-game @player_a @player_b @board)
-          another-game (with-in-str "1" (play-round game))]
+    (let [another-game (play-round @game)]
       (should= [@player_b @player_a]
-               (:players another-game)))))
+               (:players another-game))))
+
+  (it "displays the board before each round"
+    (let [other-game (play-round @game)]
+        (should-have-invoked :show-board))))
 
--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,6 +1,7 @@
 (ns tictactoe.game
   (:require [tictactoe.player :as player]
-            [tictactoe.board :as board]))
+            [tictactoe.board :as board]
+            [tictactoe.io :as io]))
 
 (defn new-game [player_a player_b board]
   {:players [player_a player_b]
@@ -9,7 +10,12 @@
 (defn play-round [game]
   (let [current-player (first (:players game))
         the-move (player/next-move current-player)
-        new-board (board/place-move (:mark current-player) the-move (:board game))]
-    {:players (reverse (:players game))
-     :board new-board}))
+        new-board (board/place-move
+                    (:mark current-player)
+                    the-move
+                    (:board game))]
+    (do
+      (io/show-board new-board)
+      {:players (reverse (:players game))
+       :board new-board})))
 
