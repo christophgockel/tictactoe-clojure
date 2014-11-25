81c5f5403cfeeab9975c7d75e81d253d1ddbe25c
Christoph Gockel <github@christophgockel.de>
2014-11-25 10:56:58 +0000

    Move single used private function to local scope

--- src/tictactoe/io.clj
+++ src/tictactoe/io.clj
@@ -2,12 +2,10 @@
   (:require [clojure.string :as string]
             [tictactoe.board :as tttboard]))
 
-(defn- format-line [line]
-  (string/join " | " line))
-
 (defn show-board [board]
-  (doseq [row (partition (tttboard/width board) board)]
-    (println (format-line row))))
+  (let [format-line (fn [line] (string/join " | " line))]
+    (doseq [row (partition (tttboard/width board) board)]
+      (println (format-line row)))))
 
 (defn show-request-for-move [next-mark]
   (println (str "Next move for " next-mark ":")))
